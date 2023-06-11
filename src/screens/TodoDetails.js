75e3e674051db73b56b5f3f739e41eb2b0b6aa9f
SanoshBebo <sanoshbebo@gmail.com>
2023-06-11 11:11:24 +0530

    check

--- src/screens/TodoDetails.js
+++ src/screens/TodoDetails.js
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet} from 'react-native';
 const TodoDetails = () => {
   return (
     <View>
-      <Text>TodoDetails</Text>
+      <Text>TodoDetailsss</Text>
     </View>
   );
 };
